cfa4c27f030183016829b00e011aedb8af353348
Dmitrii Bedenchuk <xamwork21@gmail.com>
2024-04-07 20:59:45 +0400

    1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        University university=new University("1","СПБГУ","СПбГУ",1724,StudyProfile.INFORMATICS);
+        University university=new University("1","Санкт-Петербургский государственный университет","СПбГУ",1724,StudyProfile.INFORMATICS);
         Student student=new Student("Иванов Иван Иванович","1",1,4.5f);
         System.out.println(university);
         System.out.println(student);
